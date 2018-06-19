95c1be883f6dfa572107d724cef6e5e8f83e2850
maxjf1 <maxwellsouzacarvalho@gmail.com>
2018-06-18 23:20:15 -0300

    Add instruções e recursos para desenvolvimento

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express'; 
+
+const serverPort = 3000; // porta do servidor
+
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
-app.listen(3000, () => {
-  console.log('Listening');
+app.listen(serverPort, () => {
+  console.log(`Servidor inciado na porta ${serverPort} (http://localhost:${serverPort}/)`);
 });
+
+export default app;
